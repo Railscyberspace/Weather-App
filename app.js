646b8d69e49c284f8c751daf37e05fb8cda24fa3
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2020-09-01 16:53:12 +0100

    login form

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = expres();
+
+app.get()
+app.post()
+app.post()
+app.delete()
+
+
+app.get("/", (req, res) =>
+{
+    res.send("Hello world");
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
